d304482bed15c6114acaacafecf1eef1af999c5e
Harrison McCullough <hmccullough@alpinedistrict.org>
2017-09-24 10:56:12 -0600

    Refactor list_assignments

--- list_assignments.py
+++ list_assignments.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python
 
-from googlehelper import *
+import googlehelper as gh
 import json
 
-credentials = get_credentials(SCOPE_COURSEWORK)
-service = get_service(credentials)
+credentials = gh.get_credentials(gh.SCOPE_COURSEWORK)
+service = gh.get_service(credentials)
 # course_id = '7155852796'  # Computer Programming A1
 # course_id = '7621825175'  # Robotics
 course_id = '7557587733'  # Computer Programming A4
 
-assignments = []
-page_token = None
-
-while True:
-    response = service.courses().courseWork(). \
-               list(pageToken=page_token, pageSize=100, courseId=course_id). \
-               execute()
-    assignments.extend(response.get('courseWork', []))
-    page_token = response.get('nextPageToken', None)
-    if not page_token:
-        break
+assignments = gh.download_response_list(service.courses().courseWork().list,
+                                        'courseWork', courseId=course_id,
+                                        pageSize=100)
 
 with open('assignments.txt', 'w') as f:
     for assignment in assignments:
         print(assignment['title'],
-              ",",
+              "\t",
               assignment['id'],
               file=f)
 
 with open('assignments.json', 'w') as f:
-    print(json.dumps(response, indent=2), file=f)
+    print(json.dumps(assignments, indent=2), file=f)
