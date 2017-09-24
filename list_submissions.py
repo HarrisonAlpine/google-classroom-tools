d87d99d0adddc8c0eda9daa1a80a3c79175b2367
Harrison McCullough <hmccullough@alpinedistrict.org>
2017-09-24 11:05:18 -0600

    Refactor list_submissions

--- list_submissions.py
+++ list_submissions.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
-from googlehelper import *
+import googlehelper as gh
 import json
 import os
 
-
-credentials = get_credentials(SCOPE_COURSEWORK)
-service = get_service(credentials)
-drive_credentials = get_credentials(SCOPE_DRIVE)
-drive_service = get_drive_service(drive_credentials)
+credentials = gh.get_credentials(gh.SCOPE_COURSEWORK)
+service = gh.get_service(credentials)
+drive_credentials = gh.get_credentials(gh.SCOPE_DRIVE)
+drive_service = gh.get_drive_service(drive_credentials)
 # course_id = '7155852796'  # Computer Programming A1
 # course_id = '7621825175'  # Robotics
 course_id = '7557587733'  # Computer Programming A4
@@ -17,26 +16,18 @@ course_work_id = '7672332153'  # Lab 3
 
 student_dict = {}
 
-with open('students.txt') as f:
-    data = f.read().strip()
-    ls = data.split('\n')
-    for l in ls:
-        n, i = [s.strip() for s in l.split(',')]
-        student_dict[i] = n
+with open('students.json') as f:
+    students = json.load(f)
 
-submissions = []
-page_token = None
+for student in students:
+    student_dict[student['userId']] = student['profile']['name']['fullName']
 
-while True:
-    response = service.courses().courseWork().studentSubmissions(). \
-               list(pageToken=page_token, pageSize=100, courseId=course_id,
-                    courseWorkId=course_work_id). \
-               execute()
-    submissions.extend(response.get('studentSubmissions', []))
-    page_token = response.get('nextPageToken', None)
-    if not page_token:
-        break
-
+fn = service.courses().courseWork().studentSubmissions().list
+submissions = gh.download_response_list(fn,
+                                        'studentSubmissions',
+                                        courseId=course_id,
+                                        courseWorkId=course_work_id,
+                                        pageSize=100)
 
 download_dir = 'downloads'
 if not os.path.exists(download_dir):
@@ -55,14 +46,14 @@ with open('submissions.txt', 'w') as f:
                 os.path.sep,
                 username,
                 'zip')
-            download_file(drive_service, file_id, filename)
+            gh.download_file(drive_service, file_id, filename)
             results = drive_service.files().get_media(fileId=file_id).execute()
         except KeyError:
             file_id = 'No file submitted'
         print(submission['userId'],
-              ",",
+              "\t",
               file_id,
               file=f)
 
 with open('submissions.json', 'w') as f:
-    print(json.dumps(response, indent=2), file=f)
+    print(json.dumps(submissions, indent=2), file=f)
